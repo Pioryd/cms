729d437ba1c3eeca8d6f5613dc6f6d0fb1a5d5d0
Piotr Rydwański <pioryd123@gmail.com>
2019-07-26 21:18:34 +0200

    Implemented source class of file_source module.

--- src/python/cms/convert/file_source.py
+++ src/python/cms/convert/file_source.py
@@ -1,4 +1,19 @@
 class Source(object):
 
   def __init__(self, file_name: str):
-    pass
+    self.file_name = file_name
+    self.str = ""
+    self._oryginal = ""
+
+    self.reload()
+
+  def reload(self):
+    with open(self.file_name, "r") as file:
+      self._oryginal = file.read()
+    self.str = self._oryginal
+
+  def restore(self):
+    self.str = self._oryginal
+
+  def get_oryginal(self):
+    return self._oryginal
