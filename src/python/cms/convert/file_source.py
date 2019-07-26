e4bd3e1b9a904713413ad98603d582eb45254f67
Piotr Rydwański <pioryd123@gmail.com>
2019-07-26 21:25:36 +0200

    Add move and rmove to Position of file_source module.

--- src/python/cms/convert/file_source.py
+++ src/python/cms/convert/file_source.py
@@ -43,3 +43,47 @@ class Position(object):
       self._line_number = position._line_number
       self._line_position = position._line_position
       self.source = position.source
+
+  def move(self, count: int = 1) -> bool:
+    index_last = len(self.source.str) - 1
+    if self._index + count > index_last: return False
+
+    i = 0
+    while i < count:
+      if self._index + 1 > index_last: return False
+
+      if self.get_character() == '\n':
+        self._line_number += 1
+        self._line_position = 1
+      else:
+        self._line_position += 1
+
+      self._index += 1
+
+      i += 1
+
+    return True
+
+  def rmove(self, count: int = 1) -> bool:
+    if self._index - count < 0: return False
+
+    i = 0
+    while i < count:
+      if self._index - 1 < 0: return False
+
+      if self.source.str[self._index - 1] == '\n':
+        self._line_number -= 1
+
+        # detect line lenght
+        found = self.source.str.rfind('\n', 0, self._index - 1)
+        if found == -1:
+          self._line_position = self._index
+        else:
+          self._line_position = self._index - 1 - found
+      else:
+        self._line_position -= 1
+
+      self._index -= 1
+      i += 1
+
+    return True
