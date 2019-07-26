5ba3cc868997ff4f925bb3b1e23379a3bd377c16
Piotr Rydwański <pioryd123@gmail.com>
2019-07-26 22:33:26 +0200

    Add additional functionality to util module.

--- src/python/cms/convert/util.py
+++ src/python/cms/convert/util.py
@@ -54,3 +54,39 @@ def substr(string: str, sub_index: int, sub_lenght: int = -1) -> str:
                                             sub_lenght))
 
   return string[sub_index:sub_index + sub_lenght]
+
+
+def remove_double_spaces(string: str) -> str:
+  return re.sub("\s\s+", " ", string)
+
+
+def find(string: str, sub_string: str, position: int = 0) -> int:
+  if position >= len(string) or position < 0: return -1
+  return string.find(sub_string, position)
+
+
+def rfind(string: str, sub_string: str, position: int = -1) -> int:
+  if position == -1: position = len(string) - 1
+  if position >= len(string) or position < 0: return -1
+  return string.rfind(sub_string, 0, position + 1)
+
+
+def split_by_positions(string: str, positions: 'list[str]') -> 'list[str]':
+  positions.sort()
+
+  splited = []
+  last_position = 0
+  for position in positions:
+    if position < 0 or position >= len(string):
+      raise IndexError(("\nstring:[{}]"
+                        "\nstring lenght:[{}]"
+                        "\nwrong position:[{}]"
+                        "\npositions:[{}]").format(string, len(string),
+                                                   position, positions))
+    splited.append(string[last_position:position])
+    last_position = position + 1
+
+  splited.append(string[last_position:])
+
+  splited = list(filter(None, splited))
+  return splited
