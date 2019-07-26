ca771e205e70f26dfef75031d5dad711119eb741
Piotr Rydwański <pioryd123@gmail.com>
2019-07-26 21:22:36 +0200

    Add Position class to file_source module.

--- src/python/cms/convert/file_source.py
+++ src/python/cms/convert/file_source.py
@@ -17,3 +17,29 @@ class Source(object):
 
   def get_oryginal(self):
     return self._oryginal
+
+
+class Position(object):
+
+  def __init__(self, source: Source = None, position: 'Position' = None):
+    if ((not (source and type(source) is Source)) and
+        (not (position and type(position) is Position))):
+      raise Exception("Unable to create Position."
+                      "\n\tsource is not None: {}"
+                      "\n\tsource is type of Source: {}[{}]"
+                      "\n\tposition is not None: {}"
+                      "\n\tposition is type of Position: {}[{}]".format(
+                          bool(source), bool(type(source) is Source),
+                          type(source), bool(position),
+                          bool(type(position) is Position), type(position)))
+
+    if source:
+      self._index = 0
+      self._line_number = 1
+      self._line_position = 1
+      self.source = source
+    else:
+      self._index = position._index
+      self._line_number = position._line_number
+      self._line_position = position._line_position
+      self.source = position.source
