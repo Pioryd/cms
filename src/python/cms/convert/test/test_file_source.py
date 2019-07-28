394b0ceddc0625ebad6360ea7803a8509ea49fb7
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:58:46 +0200

    Add Source test.

--- src/python/cms/convert/test/test_file_source.py
+++ src/python/cms/convert/test/test_file_source.py
@@ -11,22 +11,57 @@ from convert.file_source import Position
 
 
 class TestSource(unittest.TestCase):
+  WORKING_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+  TEST_SOURCE_PATH = WORKING_DIRECTORY + "/source_file_source.txt"
+
+  TEST_SOURCE_DATA = ("This is      string\n"
+                      "This is second line\n"
+                      "This is third line\n"
+                      "\n"
+                      "\n"
+                      "This is line number 6\n"
+                      "This is line number 7\n"
+                      "This is l(ne )um)er 8\n"
+                      "This (is (line) ((number))) 9\n"
+                      "This is line num(e( 10")
 
   @classmethod
   def setUpClass(self):
-    pass
+    self.source = Source(self.TEST_SOURCE_PATH)
 
   def setUp(self):
-    pass
+    self.source.restore()
 
   def test_init(self):
-    pass
+    with self.assertRaises(FileNotFoundError):
+      Source("Wrong_path")
+
+    source = Source(self.TEST_SOURCE_PATH)
+
+    self.assertEqual(source.str, source._oryginal)
+    self.assertEqual(source.str, self.TEST_SOURCE_DATA)
 
   def test_reload_source(self):
-    pass
+    self.source.str += "@"
+    self.source._oryginal += "@"
+
+    temp_str = self.source.str
+    temp_oryginal = self.source._oryginal
+
+    self.source.reload()
+
+    self.assertEqual(self.source.str, self.source._oryginal)
+    self.assertNotEqual(self.source.str, temp_str)
+    self.assertNotEqual(self.source._oryginal, temp_oryginal)
 
   def test_restore_source(self):
-    pass
+    self.source.str += "@"
+    temp_str = self.source.str
+
+    self.source.restore()
+
+    self.assertEqual(self.source.str, self.source._oryginal)
+    self.assertNotEqual(self.source.str, temp_str)
 
 
 class TestPosition(unittest.TestCase):
