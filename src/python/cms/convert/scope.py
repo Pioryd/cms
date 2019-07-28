c098fde04b0a331088a78e08a22f481935de1f6a
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 11:28:07 +0200

    Add block of instructions concept.

--- src/python/cms/convert/scope.py
+++ src/python/cms/convert/scope.py
@@ -1,5 +1,26 @@
 class BlocksOfInstructions(object):
-  pass
+
+  def __init__(self):
+    self.blocks = {}
+
+  def add(self, position: int, source_of_block_of_instructions: str):
+    pass
+
+  def _split_block_of_instructions(self, position: int,
+                                   source_of_block_of_instructions: str):
+    pass
+
+  def _convert_includes(self, position: int):
+    pass
+
+  def _convert_comments(self, position: int):
+    pass
+
+  def _convert_initializer_list(self, position: int):
+    pass
+
+  def _convert_inicializations(self, position: int):
+    pass
 
 
 class Header(object):
