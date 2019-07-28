a5c9bd9ebf8b63b0e4be82bc1b5a16e6234e8bb7
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:39:25 +0200

    Add TestBlocksOfInstructions.

--- src/python/cms/convert/test/test_scope.py
+++ src/python/cms/convert/test/test_scope.py
@@ -16,7 +16,19 @@ from convert.scope import Scope
 class TestBlocksOfInstructions(unittest.TestCase):
 
   def test_all(self):
-    pass
+    blocks_of_instructions = BlocksOfInstructions()
+    source_of_block_of_instructions = ("int a = 1;\n"
+                                       "cms::string b = \"hello\";\n"
+                                       "int c = a + 10;")
+    block_of_instructions = ['a = 1', 'b = \"hello\"', 'c = a + 10']
+
+    blocks_of_instructions.add(12, source_of_block_of_instructions)
+    for position, block in blocks_of_instructions.blocks.items():
+      self.assertEqual(len(block), len(block_of_instructions))
+      i = 0
+      while i < len(block):
+        self.assertEqual(block[i], block_of_instructions[i])
+        i += 1
 
 
 class TestHeader(unittest.TestCase):
