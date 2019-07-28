e385776f0d5a52f834abf3a2d9b12bb42a0988c9
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:43:46 +0200

    Add PythonSource test.

--- src/python/cms/convert/test/test_python_source.py
+++ src/python/cms/convert/test/test_python_source.py
@@ -11,5 +11,16 @@ from convert.file_source import Position
 from convert.scope import Scope
 from convert.python_source import PythonSource
 
+
+class TestPythonSource(unittest.TestCase):
+  working_directory = os.path.dirname(os.path.abspath(__file__))
+  test_source_path = working_directory + "/source_cms_syntax.cms.txt"
+
+  def test_all(self):
+    source = Source(self.test_source_path)
+    root_scope = Scope(None, Position(source))
+    python_source = PythonSource(root_scope)
+
+
 if __name__ == '__main__':
   unittest.main()
