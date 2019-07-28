cd44799faff59d03605ca6039c35d3aefb42614e
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:38:50 +0200

    Add concept to test_scope module.

--- src/python/cms/convert/test/test_scope.py
+++ src/python/cms/convert/test/test_scope.py
@@ -12,5 +12,24 @@ from convert.scope import BlocksOfInstructions
 from convert.scope import Header
 from convert.scope import Scope
 
+
+class TestBlocksOfInstructions(unittest.TestCase):
+
+  def test_all(self):
+    pass
+
+
+class TestHeader(unittest.TestCase):
+
+  def test_all(self):
+    pass
+
+
+class TestScope(unittest.TestCase):
+
+  def test_open_and_close(self):
+    pass
+
+
 if __name__ == '__main__':
   unittest.main()
