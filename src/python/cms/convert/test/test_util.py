62f7c2b33f8e8fb1d452a1cf0c31f18893198c5d
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:32:22 +0200

    Create test_util.py

---
+++ src/python/cms/convert/test/test_util.py
@@ -0,0 +1,12 @@
+import os
+import sys
+if __name__ == '__main__':
+  sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../..")
+
+import unittest
+from unittest.mock import patch
+
+import convert.util as util
+
+if __name__ == '__main__':
+  unittest.main()
