9a5b6f8a44c3272e2b4d3946345333d7d62b91a4
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:48:34 +0200

    Create test_init.py

---
+++ src/python/cms/convert/test/test_init.py
@@ -0,0 +1,14 @@
+import os
+import sys
+if __name__ == '__main__':
+  sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../..")
+
+import time
+import unittest
+from unittest.mock import patch
+import shutil
+
+import convert
+
+if __name__ == '__main__':
+  unittest.main()
