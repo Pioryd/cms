dcb8961c0d9c43f2bf2907c2b182e19f37f627f5
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:50:07 +0200

    Add test concept.

--- src/python/cms/convert/test/test_init.py
+++ src/python/cms/convert/test/test_init.py
@@ -10,5 +10,25 @@ import shutil
 
 import convert
 
+
+class TestInit(unittest.TestCase):
+
+  def remove_folder_if_exist(self, folder_full_name: str):
+    pass
+
+  def create_directory_with_files(self, directory_full_name: str,
+                                  files_cout: int) -> 'list[str]':
+    pass
+
+  def test_convert_file(self):
+    pass
+
+  def test_convert_files(self):
+    pass
+
+  def test_convert_files_recursive(self):
+    pass
+
+
 if __name__ == '__main__':
   unittest.main()
