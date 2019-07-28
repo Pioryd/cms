549a943d289eb8f1ebac00aa52070235ae59839a
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:18:49 +0200

    Add Converter class concept.

--- src/python/cms/convert/converter.py
+++ src/python/cms/convert/converter.py
@@ -1,2 +1,28 @@
+from convert.scope import Scope
+
+
 class Converter(object):
-  pass
+
+  def __init__(self, file_path: str):
+    pass
+
+  def convert(self):
+    pass
+
+  def get_python_source(self) -> str:
+    pass
+
+  def _create_scopes_by_brackets(self):
+    pass
+
+  def _set_headers_to_scopes(self, childs: 'list[Scope]'):
+    pass
+
+  def _set_header_to_scope(self, scope: Scope):
+    pass
+
+  def _set_instructions_to_scopes(self, childs: 'list<Scope>'):
+    pass
+
+  def _set_instructions_to_scope(self, scope: Scope):
+    pass
