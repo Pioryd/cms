a98aadfdddb7985898af7173206b037257eaeb6b
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 13:32:43 +0200

    Create test_converter.py

---
+++ src/python/cms/convert/test/test_converter.py
@@ -0,0 +1,12 @@
+import os
+import sys
+if __name__ == '__main__':
+  sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../..")
+
+import unittest
+from unittest.mock import patch
+
+from convert.converter import Converter
+
+if __name__ == '__main__':
+  unittest.main()
