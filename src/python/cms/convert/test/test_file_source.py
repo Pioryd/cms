1245c727de28237660ea9026ede0e595ea59031d
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:52:54 +0200

    Create test_file_source.py

---
+++ src/python/cms/convert/test/test_file_source.py
@@ -0,0 +1,13 @@
+import os
+import sys
+if __name__ == '__main__':
+  sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../..")
+
+import unittest
+from unittest.mock import patch
+
+from convert.file_source import Source
+from convert.file_source import Position
+
+if __name__ == '__main__':
+  unittest.main()
