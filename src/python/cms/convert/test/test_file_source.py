4679f4fe656009852d3c5fb9a5374bd4625dbcba
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 12:57:23 +0200

    Add test concept.

--- src/python/cms/convert/test/test_file_source.py
+++ src/python/cms/convert/test/test_file_source.py
@@ -9,5 +9,47 @@ from unittest.mock import patch
 from convert.file_source import Source
 from convert.file_source import Position
 
+
+class TestSource(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(self):
+    pass
+
+  def setUp(self):
+    pass
+
+  def test_init(self):
+    pass
+
+  def test_reload_source(self):
+    pass
+
+  def test_restore_source(self):
+    pass
+
+
+class TestPosition(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(self):
+    pass
+
+  def setUp(self):
+    pass
+
+  def test_init(self):
+    pass
+
+  def test_move_and_rmove(self):
+    pass
+
+  def test_set_positions(self):
+    pass
+
+  def test_to_string(self):
+    pass
+
+
 if __name__ == '__main__':
   unittest.main()
