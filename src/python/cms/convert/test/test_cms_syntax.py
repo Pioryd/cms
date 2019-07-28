20783ea2df256debac9261695eac66198246e989
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 13:36:04 +0200

    Add text concept of CmsSyntax.

--- src/python/cms/convert/test/test_cms_syntax.py
+++ src/python/cms/convert/test/test_cms_syntax.py
@@ -10,5 +10,18 @@ from convert.file_source import Source
 import convert.cms_syntax as cms_syntax
 from convert.cms_syntax import EncryptedString
 
+
+class TestCppSyntax(unittest.TestCase):
+
+  def test_crypt_string(self):
+    pass
+
+  def test_escape_operators(self):
+    pass
+
+  def test_find_unsupported_syntax(self):
+    pass
+
+
 if __name__ == '__main__':
   unittest.main()
