ce8b0218fca3836c2d5a9f798db671c4df8ec11c
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 11:48:05 +0200

    Add cpp string concept.

--- src/cpp/cms/string.cpp
+++ src/cpp/cms/string.cpp
@@ -2,5 +2,108 @@
 
 namespace cms
 {
+string::string() {}
 
+string::string(const string &cpp_string) {}
+
+string::string(char character) {}
+
+string::string(const char *c_string) {}
+
+string::string(const std::string &cpp_string) {}
+
+string::operator std::string() const { return ""; }
+
+string::operator const std::string &() { return ""; }
+
+char string::operator[](unsigned i) const { return 0; }
+
+char &string::operator[](unsigned i)
+{
+  char a;
+  return a;
+}
+
+string &string::operator=(const string &cpp_string)
+{
+  return *this;
+}
+
+string &string::operator+=(const string &cpp_string)
+{
+  return *this;
+}
+
+string
+operator+(const string &lhs, const string &rhs)
+{
+  return "";
+}
+
+string operator+(const string &lhs, char rhs)
+{
+  return "";
+}
+
+string operator+(const string &lhs, const char *rhs)
+{
+  return "";
+}
+
+string operator+(char lhs, const string &rhs) { return ""; }
+
+string operator+(const char *lhs, const string &rhs)
+{
+  return "";
+}
+
+bool operator==(const string &lhs, const string &rhs)
+{
+  return false;
+}
+
+bool operator==(const string &lhs, char rhs)
+{
+  return false;
+}
+
+bool operator==(const string &lhs, const char *rhs)
+{
+  return false;
+}
+
+bool operator==(char lhs, const string &rhs)
+{
+  return false;
+}
+
+bool operator==(const char *lhs, const string &rhs)
+{
+  return false;
+}
+
+bool operator!=(const string &lhs, const string &rhs)
+{
+  return false;
+}
+
+bool operator!=(const string &lhs, char rhs)
+{
+  return false;
+}
+
+bool operator!=(const string &lhs, const char *rhs)
+{
+  return false;
+}
+
+bool operator!=(char lhs, const string &rhs)
+{
+  return false;
+}
+
+bool operator!=(const char *lhs, const string &rhs)
+{
+  return false;
+}
 } // namespace cms
--- src/cpp/cms/string.h
+++ src/cpp/cms/string.h
@@ -7,6 +7,43 @@ namespace cms
 {
 class string
 {
+public:
+  string();
+  string(const string &cpp_string);
+  string(char);
+  string(const char *c_string);
+  string(const std::string &cpp_string);
+
+  operator std::string() const;
+  operator const std::string &();
+
+  char operator[](unsigned i) const;
+  char &operator[](unsigned i);
+
+  string &operator=(const string &s);
+
+  string &operator+=(const string &s);
+
+  friend string operator+(const string &lhs, const string &rhs);
+  friend string operator+(const string &lhs, char rhs);
+  friend string operator+(const string &lhs, const char *rhs);
+  friend string operator+(char lhs, const string &rhs);
+  friend string operator+(const char *lhs, const string &rhs);
+
+  friend bool operator==(const string &lhs, const string &rhs);
+  friend bool operator==(const string &lhs, char rhs);
+  friend bool operator==(const string &lhs, const char *rhs);
+  friend bool operator==(char lhs, const string &rhs);
+  friend bool operator==(const char *lhs, const string &rhs);
+
+  friend bool operator!=(const string &lhs, const string &rhs);
+  friend bool operator!=(const string &lhs, char rhs);
+  friend bool operator!=(const string &lhs, const char *rhs);
+  friend bool operator!=(char lhs, const string &rhs);
+  friend bool operator!=(const char *lhs, const string &rhs);
+
+private:
+  std::string std_string_;
 };
 } // namespace cms
 
