5ddb188a5f5e2e6671479c46ab15a705a38e2e40
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 12:02:11 +0200

    Update immutable__.h

--- src/cpp/cms/immutable__.h
+++ src/cpp/cms/immutable__.h
@@ -13,24 +13,32 @@ public:
       : references_(1), object__pointer_(object__pointer) {}
   virtual ~base__() {}
 
-  void increase_reference() {}
+  void increase_reference() { ++references_; }
   void decrease_reference()
   {
+    if (--references_ == 0)
+    {
+      delete object__pointer_;
+      object__pointer_ = nullptr;
+      delete this;
+    }
   }
-}
 
-void
-set(T *new_object__pointer)
-{
-}
-T *get() {}
+  void set(T *new_object__pointer)
+  {
+    T *previous_object__pointer = object__pointer_;
+    object__pointer_ = new_object__pointer;
+    if (previous_object__pointer)
+      delete previous_object__pointer;
+  }
+  T *get() { return object__pointer_; }
 
-long use_count() { return 0; }
+  long use_count() { return references_; }
 
 private:
-std::atomic<long> references_;
-std::atomic<T *> object__pointer_;
-}; // namespace cms
+  std::atomic<long> references_;
+  std::atomic<T *> object__pointer_;
+};
 
 template <class T>
 class immutable__
@@ -41,36 +49,43 @@ public:
   immutable__() : base(nullptr) {}
   immutable__(T *object__pointer)
   {
+    base =
+        (object__pointer != nullptr) ? new base__<T>(object__pointer) : nullptr;
   }
   immutable__(immutable__ const &rhs) : base(rhs.base)
   {
+    if (base != nullptr)
+      base->increase_reference();
   }
   ~immutable__()
   {
+    if (base != nullptr)
+      base->decrease_reference();
   }
 
-  T &operator*() const { return nullptr; }
-  T *operator->() const { return nullptr; }
+  T &operator*() const { return *base->get(); }
+  T *operator->() const { return base->get(); }
 
   immutable__ &operator=(immutable__ const &rhs)
   {
-
+    immutable__(rhs).swap_(*this);
     return *this;
   }
   immutable__ &operator=(T *rhs)
   {
+    immutable__(rhs).swap_(*this);
     return *this;
   }
 
   explicit operator bool()
   {
-    return false;
+    return base == nullptr ? nullptr : &immutable__::base;
   }
 
-  bool operator!() const { return false; }
+  bool operator!() const { return base == nullptr; }
 
-  T *object_() { return nullptr; }
-  void swap_(immutable__ &rhs) {}
+  T *object_() { return base->get(); }
+  void swap_(immutable__ &rhs) { std::swap(base, rhs.base); }
 
 protected:
   base__<T> *base;
