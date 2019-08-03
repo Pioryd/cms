463b20ba23fd3a9c0d334e84871a72b323c0bbaa
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 12:14:29 +0200

    Add cms namespace functionality.

--- src/cpp/cms/cms.cpp
+++ src/cpp/cms/cms.cpp
@@ -2,5 +2,8 @@
 
 namespace cms
 {
+cms::string str(int64_t value) { return std::to_string(value); }
+
+int64_t len(const cms::string &string) { return ((std::string)string).size(); }
 
 } // namespace cms
--- src/cpp/cms/cms.h
+++ src/cpp/cms/cms.h
@@ -7,7 +7,31 @@
 
 namespace cms
 {
+cms::string str(int64_t value);
 
+template <typename T>
+int64_t len(immutable_list<T> &list)
+{
+  return list.object_()->size();
+}
+
+int64_t len(const cms::string &string);
+
+template <typename T>
+void copy(immutable_list<T> &to_list, immutable_list<T> &from_list)
+{
+  to_list.extend(from_list);
+}
+template <typename T, typename U>
+int64_t len(immutable_dict<T, U> &dict)
+{
+  return dict.object_()->size();
+}
+template <typename T, typename U>
+void copy(immutable_dict<T, U> &to_list, immutable_dict<T, U> &from_list)
+{
+  to_list.extend(from_list);
+}
 } // namespace cms
 
 #endif // #ifndef FW_CMS_CMS_H
