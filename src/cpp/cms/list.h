00e3bc93e53f46acfd73be6cc3a9344adb9ade7c
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 12:02:41 +0200

    Create list.h

---
+++ src/cpp/cms/list.h
@@ -0,0 +1,13 @@
+#ifndef FW_CMS_IMMUTABLE_LIST_H
+#define FW_CMS_IMMUTABLE_LIST_H
+
+#include <functional>
+#include <vector>
+#include "immutable__.h"
+
+namespace cms
+{
+
+} // namespace cms
+
+#endif // #ifndef FW_CMS_IMMUTABLE_LIST_H
