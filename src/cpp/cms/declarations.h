95aa18f7ada70b0e2b6da49b5d4fcd99f6a2cf5b
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 12:15:14 +0200

    Update declarations.h

--- src/cpp/cms/declarations.h
+++ src/cpp/cms/declarations.h
@@ -1,4 +1,14 @@
 #ifndef FW_CMS_DECLARATIONS_H
 #define FW_CMS_DECLARATIONS_H
 
+#include "cms.h"
+#include "dict.h"
+#include "list.h"
+#include "string.h"
+
+#define BIND(...) std::bind(__VA_ARGS__)
+#define INITIALIZER_LIST(...) \
+  {                           \
+    __VA_ARGS__               \
+  }
 #endif // #ifndef FW_CMS_DECLARATIONS_H
