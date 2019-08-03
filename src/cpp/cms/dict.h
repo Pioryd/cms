e087093a44710e80d8dbc0c6341edd91e77ed450
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 12:09:15 +0200

    Create dict.h

---
+++ src/cpp/cms/dict.h
@@ -0,0 +1,14 @@
+#ifndef FW_CMS_IMMUTABLE_DICT_H
+#define FW_CMS_IMMUTABLE_DICT_H
+
+#include <functional>
+#include <map>
+#include "immutable__.h"
+#include "list.h"
+
+namespace cms
+{
+
+} // namespace cms
+
+#endif // #ifndef FW_CMS_IMMUTABLE_DICT_H
