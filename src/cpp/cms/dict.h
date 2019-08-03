b29f4ed6e7eebc8653bfe241b709a9249c974801
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 12:11:18 +0200

    Update dict.h

--- src/cpp/cms/dict.h
+++ src/cpp/cms/dict.h
@@ -21,27 +21,34 @@ public:
   */
   immutable_dict(const immutable_dict &list_to_copy) = delete;
 
-  operator std::map<T, U> &() const { return std::map<T, U>; };
-  operator const std::map<T, U> &() { return std::map<T, U>; };
+  operator std::map<T, U> &() const { return *object_(); };
+  operator const std::map<T, U> &() { return *object_(); };
 
-  U operator[](T key) const { return U; }
-  U &operator[](T key) { return U; }
+  U operator[](T key) const { return (*object_())[key]; }
+  U &operator[](T key) { return (*object_())[key]; }
 
-  void clear() {}
+  void clear() { object_()->clear(); }
 
-  U get(T &key) { return U; }
+  U get(T &key) { return (*object_())[key]; }
 
   void remove(T element)
   {
+    it = std::find(object_()->begin(), object_()->end(), element);
+    if (it != object_()->end())
+      object_()->erase(it);
   }
 
-  void pop(int64_t position) {}
-  void popitem() {}
+  void pop(int64_t position) { object_()->erase(object_()->begin() + position); }
+  void popitem() { object_()->erase(object_()->end() - 1); }
   void update(std::initializer_list<std::pair<const T, U>> init_list)
   {
+    for (auto &[key, value] : init_list)
+      (*object_())[key] = value;
   }
   void update(immutable_dict &dict)
   {
+    for (auto &[key, value] : (*dict.object_()))
+      (*object_())[key] = value;
   }
 
 private:
@@ -51,23 +58,35 @@ private:
 template <typename T, typename U>
 T dict_key(immutable_dict<T, U> &dict, int64_t position)
 {
-  return T;
+  auto it = (*dict.object_()).begin();
+  if (position > 0)
+    std::advance(it, position);
+  return it->first;
 }
 template <typename T, typename U>
 U dict_value(immutable_dict<T, U> &dict, int64_t position)
 {
-  return U;
+  auto it = (*dict.object_()).begin();
+  if (position > 0)
+    std::advance(it, position);
+  return it->second;
 }
 
 template <typename T, typename U>
 immutable_list<T> keys(immutable_dict<T, U> &dict)
 {
-  return immutable_list<T>;
+  immutable_list<T> keys;
+  for (auto const &[key, value] : (*dict.object_()))
+    keys.append(key);
+  return keys;
 }
 template <typename T, typename U>
 immutable_list<U> values(immutable_dict<T, U> &dict)
 {
-  return immutable_list<U>;
+  immutable_list<U> values;
+  for (auto const &[key, value] : (*dictnobject_()))
+    values.append(value);
+  return values;
 }
 } // namespace cms
 
